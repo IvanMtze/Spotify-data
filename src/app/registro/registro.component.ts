9969847e86fcf515be6d6ba476c5a2a33544b8be
iGusky <gustavohdezmorales@gmail.com>
2020-10-11 23:40:30 -0500

    Formulario de registro añadido

--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -1,15 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-registro',
   templateUrl: './registro.component.html',
-  styleUrls: ['./registro.component.scss']
+  styleUrls: ['./registro.component.scss'],
 })
 export class RegistroComponent implements OnInit {
 
+  correo: string = '';
+  correo_confirm: string = '';
+  contra: string = '';
+  nombre:  string = '';
+  dia: string = '';
+  mes: string = '';
+  anio: string = '';
+  sexo: string = '';
+  
   constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}  
+  registrarUsuario($event){
+    
+    let usuario = {
+      correo: this.correo,
+      correo2: this.correo_confirm,
+      pass: this.contra,
+      nombre: this.nombre,
+      fnac:{
+        dia:this.dia,
+        mes:this.mes,
+        anio:this.anio
+      },
+      sexo: this.sexo
+    }
+    console.log(usuario)
   }
-
 }
+
